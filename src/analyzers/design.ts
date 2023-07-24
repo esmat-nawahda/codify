a58dec055d67f6bfb0725850d51f3a548b2fc97d
esmatnawahda0 <99367728+esmatnawahda0@users.noreply.github.com>
2023-07-24 17:05:21 +0300

    add more cases to the design analyzer

--- src/analyzers/design.ts
+++ src/analyzers/design.ts
@@ -1,10 +1,17 @@
 import { parseScript } from 'esprima';
+import { Project, SyntaxKind } from 'ts-morph';
 
 interface Issue {
     message: string;
     line: number | undefined;
 }
 
+/**
+ * Analyze the design of a piece of code.
+ * 
+ * @param {string} code - The code to analyze
+ * @returns {Issue[]} A list of issues found in the code
+ */
 function analyzeDesign(code: string): Issue[] {
     const issues: Issue[] = [];
     const ast = parseScript(code, { comment: true });
@@ -18,6 +25,36 @@ function analyzeDesign(code: string): Issue[] {
             });
         }
     });
+
+    const project = new Project({
+        useInMemoryFileSystem: true,
+    });
+
+    const sourceFile = project.createSourceFile('file.ts', code);
+
+    // Analyze large functions
+    const functions = sourceFile.getFunctions();
+    for (const fn of functions) {
+        const lines = fn.getEndLineNumber() - fn.getStartLineNumber();
+        if (lines > 50) {
+            issues.push({
+                message: `The function "${fn.getName()}" is quite long (${lines} lines). Consider breaking it up into smaller functions.`,
+                line: fn.getStartLineNumber(),
+            });
+        }
+    }
+
+    // Analyze deeply nested code
+    const blocks = sourceFile.getDescendantsOfKind(SyntaxKind.Block);
+    for (const block of blocks) {
+        const depth = block.getDescendantsOfKind(SyntaxKind.Block).length;
+        if (depth > 4) {
+            issues.push({
+                message: 'This block of code is deeply nested. Consider flattening it for readability.',
+                line: block.getStartLineNumber(),
+            });
+        }
+    }
 
     return issues;
 }
